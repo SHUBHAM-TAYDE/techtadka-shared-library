2e26d789982f7c2ad665f4575ae36bbf26fa943b
Shubham Tayde <tayde.s.shubham@gmail.com>
2026-04-20 16:38:27 +0530

    Implement Docker deployment function
    Added a function to deploy a Docker container with a specified image name.

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,1 +1,9 @@
-sfadg
+def call(String imageName) {
+    echo "Deploying container..."
+
+    sh """
+    docker stop techtadka || true
+    docker rm techtadka || true
+    docker run -d -p 80:80 --name techtadka ${imageName}
+    """
+}
